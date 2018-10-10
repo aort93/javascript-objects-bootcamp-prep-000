7aa82b734818bd4a20223c722682f95793b50f13
Alonso Ortega <alonsojortega@gmail.com>
2018-10-10 20:39:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {linkinPark:"Numb", AvengedSevenfold:"Doomsday"};
 
+<<<<<<< HEAD
 //function callBack (playlist){
   //return playlist;
 //}
@@ -12,6 +13,20 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
+=======
+function callBack (playlist){
+  return playlist;
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+  //playlist[artistName] = songTitle;
+  //return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  //delete playlist.artistName;
+>>>>>>> 03005688196b2b57db877c60328d996292c99329
   return playlist;
 
 }
@@ -19,6 +34,10 @@ function removeFromPlaylist(playlist, artistName){
 var artistName = "hi";
 var songTitle = 'doom';
 
+<<<<<<< HEAD
 //console.log(callBack(playlist));
+=======
+console.log(callBack(playlist));
+>>>>>>> 03005688196b2b57db877c60328d996292c99329
 console.log(updatePlaylist(playlist, artistName, songTitle));
 console.log(removeFromPlaylist(playlist,artistName));
